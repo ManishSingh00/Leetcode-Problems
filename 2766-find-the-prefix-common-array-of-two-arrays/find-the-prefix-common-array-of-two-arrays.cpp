b3d4cb176619fc8e7fdd8ddd27c266a1b16991a0
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-01-14 10:24:29 +0530

    Time: 3 ms (75.81%) | Memory: 88.4 MB (38.01%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -4,12 +4,22 @@ public:
         int n = a.size();
         unordered_set<int>st;
         vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     st.insert(a[i]);
+        //     int cnt=0;
+        //     for(int j=0;j<=i;j++){
+        //         if(st.count(b[j])!=0) cnt++;
+        //     }
+        //     ans.push_back(cnt);
+        // }
+
+        int cnt=0;
         for(int i=0;i<n;i++){
-            st.insert(a[i]);
-            int cnt=0;
-            for(int j=0;j<=i;j++){
-                if(st.count(b[j])!=0) cnt++;
-            }
+            if(st.count(a[i])!=0) cnt++;
+            else st.insert(a[i]);
+            if(st.count(b[i])!=0) cnt++;
+            else st.insert(b[i]);
+
             ans.push_back(cnt);
         }
         return ans;
