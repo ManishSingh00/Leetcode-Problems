f1c636f9f8a3e832752172b51c1619442edb5893
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-23 21:19:16 +0530

    Time: 16 ms (87.03%) | Memory: 30.4 MB (37.94%) - LeetSync

---
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,39 @@
+#define ll long long
+class Solution {
+public:
+    int dx[4]={-1,1,0,0};
+    int dy[4]={0,0,1,-1};
+    int minTimeToReach(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<long long>>dist(n,vector<long long>(m,LLONG_MAX));
+
+        priority_queue<
+            tuple<long long,int,int>,
+            vector<tuple<long long,int,int>>,
+            greater<tuple<long long,int,int>>
+        > pq;
+        dist[0][0] = 0;
+        pq.emplace(0LL, 0, 0);
+
+        while(pq.size()>0){
+            auto [t, x, y] = pq.top();
+            pq.pop();
+            if (t > dist[x][y]) continue;
+            if(x==n-1 && y==m-1) return (int)t;
+
+            for(int k=0;k<4;k++){
+                int nr = x+dx[k];
+                int nc = y+dy[k];
+                if(nr>=0 && nr<n && nc>=0 && nc<m){
+                    int waiting_time=max(0,grid[nr][nc]-(int)t);
+                    int new_time = t+waiting_time+1;
+                    if(new_time<dist[nr][nc]){
+                        dist[nr][nc]=new_time;
+                        pq.push({new_time,nr,nc});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
