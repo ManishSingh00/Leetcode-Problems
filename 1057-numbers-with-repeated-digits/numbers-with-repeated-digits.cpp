889f3877982b13c4e2fd8ffd53e488477db726af
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-08-15 23:37:22 +0530

    Time: 11 ms (40.59%) | Memory: 8.3 MB (36.51%) - LeetSync

--- 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.cpp
+++ 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     string s;
-    int dp[12][2][1<<10][2];
-    int f(int idx,bool tight,int mask,bool leading_zero,int sz){ // counting number of integerws which have all unique digits
+    int dp[12][2][1<<10][2][2];
+    int f(int idx,bool tight,int mask,bool flag,bool leading_zero,int sz){ // counting number of integerws which have all unique digits
         if(idx == sz){
-            if(leading_zero == false) return 1;
+            if(flag == true && leading_zero == false) return 1;
             else return 0;
         }
-        if(dp[idx][tight][mask][leading_zero]!= -1) return dp[idx][tight][mask][leading_zero];
+        if(dp[idx][tight][mask][flag][leading_zero]!= -1) return dp[idx][tight][mask][flag][leading_zero];
 
         int ans = 0;
         int ub = (tight==0)?9:s[idx]-'0';
@@ -16,20 +16,21 @@ public:
             bool new_tight = tight && (dig == ub);
 
             if(leading_zero == true && dig == 0){
-                ans += f(idx+1,new_tight,mask,true,sz);
+                ans += f(idx+1,new_tight,mask,false,true,sz);
             }
 
             else if((mask & (1<<dig)) != 0){
-                continue;
+                ans += f(idx+1,new_tight,mask,true,false,sz);
+                // continue;
             }
-            else ans += f(idx+1,new_tight,mask|(1<<dig),false,sz);
+            else ans += f(idx+1,new_tight,mask|(1<<dig),flag,false,sz);
         }
 
-        return dp[idx][tight][mask][leading_zero] = ans;
+        return dp[idx][tight][mask][flag][leading_zero] = ans;
     }
     int numDupDigitsAtMostN(int n) {
         s = to_string(n);
         memset(dp,-1,sizeof dp);
-        return n - f(0,true,0,true,s.length());
+        return f(0,true,0,false,true,s.length());
     }
 };
