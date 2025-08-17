5ece81d11a830c2164035e6713ccdb4a4cbcdc48
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-08-17 23:24:23 +0530

    Time: 40 ms (63.09%) | Memory: 24.1 MB (69.27%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -0,0 +1,83 @@
+#define ll long long int
+class Solution {
+public:
+     struct hasher{
+        int n, _k, _p;
+        vector<int> powk,base,rev;
+        
+        void init(string s,int k,int p){
+            _k = k, _p = p;
+            n = s.length();
+            powk.resize(n+1);
+            base.resize(n+1);
+            
+            // precompute
+            base[0] = 0;
+            powk[0] = 1;
+            for(int i=0;i<n;i++){
+                int num = (s[i]-'a'+1 )%p;
+                base[i+1] = (1LL *base[i] * k + num)%p;
+                powk[i+1] = (1LL* k * powk[i]) %p;
+            }
+            
+            // reverse
+            rev.resize(n+1);
+            rev[n] = 0;
+            for(int i=n-1;i>=0;i--){
+                int num = (s[i]-'a'+1 )%p;
+                rev[i] = (1LL *rev[i+1] * k + num)%p;
+            }
+        }  
+        int gethash(int l,int r){ // 0 index
+            int ans = (base[r+1] - 1LL *base[l]*powk[(r-l+1)])%_p;
+            return (ans+_p)%_p;
+        }
+        // reverse hash
+        int getrevhash(int l,int r){
+            int ans = (rev[l] - 1LL *rev[r+1]*powk[(r-l+1)])%_p;
+            return (ans+_p)%_p;
+        }
+    };
+    
+    struct double_hash{
+        hasher a,b;
+        void init(string s){
+            a.init(s,37,1000000021);
+            b.init(s,39,1000000009);
+        }
+        pair<int,int> gethash(int l,int r){
+            return {a.gethash(l,r),b.gethash(l,r)};
+        }
+        pair<int,int> getrevhash(int l,int r){
+            return {a.getrevhash(l,r),b.getrevhash(l,r)};
+        }
+        bool is_palin(int l,int r){
+            return gethash(l,r) == getrevhash(l,r);
+        }
+    };
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n = s.length();
+        double_hash dh;
+        dh.init(s);
+
+        set<string>st;
+        unordered_map<ll,int>mp;
+        for(int i=0;i+10 <= n;i++){
+            auto h = dh.gethash(i, i + 10 - 1);
+            ll key = 1LL * h.first * 1000000021 + h.second; 
+
+            if(mp.find(key)!=mp.end()){
+                st.insert(s.substr(i,10));
+            }
+            mp[key] = i; 
+        }
+
+
+        vector<string>ans;
+        for(auto x:st){
+            ans.push_back(x);
+        }
+
+        return ans;
+    }
+};
