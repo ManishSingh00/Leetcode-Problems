8606d68ee5dbb8734ec601ae3213008d4e94103d
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-08-21 13:58:15 +0530

    Time: 492 ms (61.83%) | Memory: 158.2 MB (67.13%) - LeetSync

---
+++ 327-count-of-range-sum/count-of-range-sum.cpp
@@ -0,0 +1,30 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+using namespace __gnu_pbds;
+
+using ordered_set = tree<pair<ll,int>, null_type, less<pair<ll,int>>, rb_tree_tag, tree_order_statistics_node_update>;
+class Solution {
+public:
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        ordered_set os;
+        int id = 0;
+        os.insert({0,id++});
+        ll pre = 0;
+        ll ans = 0;
+        for(int i=0;i<n;i++){
+            pre += nums[i];
+
+            ll left = pre - upper;
+            ll right = pre - lower;
+
+            ll cnt = os.order_of_key({right,INT_MAX}) - os.order_of_key({left,INT_MIN});
+            ans += cnt;
+
+            os.insert({pre,id++});
+        }
+
+        return ans;
+    }
+};
