777c39d6173d7dc9ab189ea87655912b5a4331b2
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-10-24 09:42:24 +0530

    Time: 335 ms (56.72%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,16 @@
 # Write your MySQL query statement below
-SELECT emp.name FROM Employee emp
-JOIN(
-    SELECT managerId FROM Employee
-    WHERE managerId IS NOT NULL
-    GROUP BY managerId
-    HAVING COUNT(*) >= 5
-) temp
-ON emp.id = temp.managerId
+-- SELECT emp.name FROM Employee emp
+-- JOIN(
+--     SELECT managerId FROM Employee
+--     WHERE managerId IS NOT NULL
+--     GROUP BY managerId
+--     HAVING COUNT(*) >= 5
+-- ) temp
+-- ON emp.id = temp.managerId
+
+SELECT emp1.name 
+FROM Employee emp1
+JOIN Employee emp2
+ON emp1.id = emp2.managerId
+GROUP BY emp2.managerId
+HAVING COUNT(*)>=5
